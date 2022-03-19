7aedf1f8f782a3d40031afa61596a3ccb64bf1ec
Magnus Lundvall <lundvall@gmail.com>
2022-03-19 12:12:00 +0100

    Log init from BaseTest constructor

--- src/test/java/io/cucumber/examples/calculator/BaseTest.java
+++ src/test/java/io/cucumber/examples/calculator/BaseTest.java
@@ -1,18 +1,17 @@
 package io.cucumber.examples.calculator;
 
 import io.cucumber.logging.Config;
-import org.junit.jupiter.api.BeforeAll;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class BaseTest {
 
-    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
-
-    @BeforeAll
-    static void beforeAll(){
-        log.warn("Log from 'beforeAll()' inside BaseTest");
+    public BaseTest() {
         Config.initTestLogging();
+        System.err.println("Inside constructor for BaseTest");
     }
+
+//    static {
+//        Config.initTestLogging();
+//        System.err.println("Inside static block!");
+//    }
 
 }
--- src/test/java/io/cucumber/logging/Config.java
+++ src/test/java/io/cucumber/logging/Config.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.core.config.Configurator;
 public class Config {
 
     public static void initTestLogging(){
+        System.err.println("initTestLogging()");
         Configurator.setAllLevels("", Level.ALL);
     }
 
